204a79a50cc796943c27a4608f7e991df15fc039
Aakarsh Chopra <aakarsh@startree.ai>
2024-05-25 20:48:39 +0530

    move to src

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
+import "@/src/style/globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import type { Metadata } from "next";
-import "./globals.css";
-import { openSans } from "./init";
-import { theme } from "./theme";
+import { openSans } from "../src/style/init";
+import { theme } from "../src/style/theme";
 
 export const metadata: Metadata = {
   title: "Time Manager",
