95bce683930a027d9516d3fd4f82bf765534ecaf
Aakarsh Chopra <aakarsh@startree.ai>
2024-08-22 15:06:35 +0530

    build fix

--- app/admin/entity/page.tsx
+++ app/admin/entity/page.tsx
@@ -1,7 +1,5 @@
-export default async function Page({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
+import { ReactNode } from "react";
+
+export default async function Page({ children }: { children: ReactNode }) {
+    return children;
 }
